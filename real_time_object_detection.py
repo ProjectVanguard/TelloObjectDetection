90f931ebcc0e8985a0eae48427f43e03a36497d1
Daniel Alfaro R <daniel.alfaro@upr.edu>
2020-10-14 19:52:34 -0400

    initial commit

---
+++ real_time_object_detection.py
@@ -0,0 +1,144 @@
+# Reference : https://www.pyimagesearch.com/2017/09/18/real-time-object-detection-with-deep-learning-and-opencv/
+#
+# USAGE
+# python real_time_object_detection.py
+
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import socket
+
+#************************************TELLO********************************************
+
+# telloFlying = False;
+
+# # IP and port of Tello
+# tello_address = ('192.168.10.1', 8889)
+
+# # Create a UDP connection that we'll send the command to
+# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# # Bind to a local port on our machine where Tello can send messages
+# sock.bind(('', 9000))
+
+# # Function to send messages to Tello
+# def send(message):
+#   try:
+#     sock.sendto(message.encode(), tello_address)
+#     print("Sending message: " + message)
+#   except Exception as e:
+#     print("Error sending: " + str(e))
+
+# # Function that listens for messages from Tello and prints them to the screen
+# def receive():
+#   try:
+#     response, ip_address = sock.recvfrom(128)
+#     print("Received message: " + response.decode(encoding='utf-8') + " from Tello with IP: " + str(ip_address))
+#   except Exception as e:
+#     print("Error receiving: " + str(e))
+
+# # Send Tello into command mode
+# send("command")
+# # Receive response from Tello
+# receive()
+# # Send Tello into streaming mode
+# send("streamon")
+# # Receive response from Tello
+# receive()
+
+#************************************TELLO********************************************
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+# ap.add_argument("-m", "--model", required=True,
+# 	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# initialize the list of class labels MobileNet SSD was trained to
+# detect, then generate a set of bounding box colors for each class
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe("MobileNetSSD_deploy.prototxt.txt", "MobileNetSSD_deploy.caffemodel")
+
+# initialize the video stream, allow the cammera sensor to warmup,
+# and initialize the FPS counter
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+# vs = WebcamVideoStream(src=0).start()
+time.sleep(2.0)
+fps = FPS().start()
+
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 400 pixels
+	frame = vs.read()
+	frame = imutils.resize(frame, width=400)
+
+	# grab the frame dimensions and convert it to a blob
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+		0.007843, (300, 300), 127.5)
+
+	# pass the blob through the network and obtain the detections and
+	# predictions
+	net.setInput(blob)
+	detections = net.forward()
+
+	# loop over the detections
+	for i in np.arange(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with
+		# the prediction
+		confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections by ensuring the `confidence` is
+		# greater than the minimum confidence
+		if confidence > args["confidence"]:
+			# extract the index of the class label from the
+			# `detections`, then compute the (x, y)-coordinates of
+			# the bounding box for the object
+			idx = int(detections[0, 0, i, 1])
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			# draw the prediction on the frame
+			label = "{}: {:.2f}%".format(CLASSES[idx],
+				confidence * 100)
+			cv2.rectangle(frame, (startX, startY), (endX, endY),
+				COLORS[idx], 2)
+			y = startY - 15 if startY - 15 > 15 else startY + 15
+			cv2.putText(frame, label, (startX, y),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+	# update the FPS counter
+	fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
